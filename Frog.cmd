bb2be7a0f89fbe2708f453bf46ba0a956e682d95
Jorge Bahamonde <jbahamon@ug.uchile.cl>
2015-02-03 18:52:44 -0300

    Added Ground Stand Checkmate

--- Frog.cmd
+++ Frog.cmd
@@ -356,32 +356,32 @@ time=15
 [Command]
 name = "dfa"
 command = ~F, D, DF, a
-time=20
+time=15
 
 [Command]
 name = "dfb"
 command =  ~F, D, DF, b
-time=20
+time=15
 
 [Command]
 name = "dfc"
 command = ~F, D, DF, c
-time=20
+time=15
 
 [Command]
 name = "dfa"
 command = ~F, D, DF, ~a
-time=20
+time=15
 
 [Command]
 name = "dfb"
 command =  ~F, D, DF, ~b
-time=20
+time=15
 
 [Command]
 name = "dfc"
 command = ~F, D, DF, ~c
-time=20
+time=15
 
 ; ---
 
@@ -640,6 +640,7 @@ trigger1 = Var(23) && NumHelper(10000)
 trigger1 = Helper(10000), StateNo = 17011
 trigger2 = !Var(23) && !NumHelper(10000) && StateType != A
 value = IfElse(Var(23) && NumHelper(10000), 15010, 15000)
+
 
 
 ;----------------------Specials----------------------
